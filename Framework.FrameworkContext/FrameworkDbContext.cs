b5d9fe38428c58bd85d02780ec0f9918558ae067
thinhpv1995 <tv.cntt.95@gmail.com>
2017-12-26 14:51:26 +0700

    Update
    Signed-off-by: thinhpv1995 <tv.cntt.95@gmail.com>

--- Framework.FrameworkContext/FrameworkDbContext.cs
+++ Framework.FrameworkContext/FrameworkDbContext.cs
@@ -79,62 +79,73 @@ namespace Framework.FrameworkContext
                 .Insert(u => u.HasName("InsertFriend", "dbo"))
                 .Update(u => u.HasName("UpdateFriend", "dbo"))
                 .Delete(u => u.HasName("DeleteFriend", "dbo")));
-            builder.Entity<Notification>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertNotification", "dbo"))
-            .Update(u => u.HasName("UpdateNotification", "dbo"))
-            .Delete(u => u.HasName("DeleteNotification", "dbo")));
-            builder.Entity<Post>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertPost", "dbo"))
-            .Update(u => u.HasName("UpdatePost", "dbo"))
-            .Delete(u => u.HasName("DeletePost", "dbo")));
+            builder.Entity<Notification>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertNotification", "dbo"))
+                .Update(u => u.HasName("UpdateNotification", "dbo"))
+                .Delete(u => u.HasName("DeleteNotification", "dbo")));
+            builder.Entity<Post>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertPost", "dbo"))
+                .Update(u => u.HasName("UpdatePost", "dbo"))
+                .Delete(u => u.HasName("DeletePost", "dbo")));
             builder.Entity<Post>().HasIndex("IX_Post",
                        e => e.Property(x => x.Status),
                        e => e.Property(x => x.CreatedDate));
-
-
-            builder.Entity<Comment>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertComment", "dbo"))
-            .Update(u => u.HasName("UpdateComment", "dbo"))
-            .Delete(u => u.HasName("DeleteComment", "dbo")));
-            builder.Entity<PostVoteDetail>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertPostVoteDetail", "dbo"))
-            .Update(u => u.HasName("UpdatePostVoteDetail", "dbo"))
-            .Delete(u => u.HasName("DeletePostVoteDetail", "dbo")));
-            builder.Entity<CommentVoteDetail>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertCommentVoteDetail", "dbo"))
-            .Update(u => u.HasName("UpdateCommentVoteDetail", "dbo"))
-            .Delete(u => u.HasName("DeleteCommentVoteDetail", "dbo")));
-            builder.Entity<DetailOurWord>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertDetailOurWord", "dbo"))
-            .Update(u => u.HasName("UpdateDetailOurWord", "dbo"))
-            .Delete(u => u.HasName("DeleteDetailOurWord", "dbo")));
-            builder.Entity<PostType>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertPostType", "dbo"))
-            .Update(u => u.HasName("UpdatePostType", "dbo"))
-            .Delete(u => u.HasName("DeletePostType", "dbo")));
-            builder.Entity<DetailUserType>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertDetailUserType", "dbo"))
-            .Update(u => u.HasName("UpdateDetailUserType", "dbo"))
-            .Delete(u => u.HasName("DeleteDetailUserType", "dbo")));
-            builder.Entity<HaveSendQuestion>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertHaveSendQuestion", "dbo"))
-            .Update(u => u.HasName("UpdateHaveSendQuestion", "dbo"))
-            .Delete(u => u.HasName("DeleteHaveSendQuestion", "dbo")));
-            builder.Entity<DictCache>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertDictCache", "dbo"))
-            .Update(u => u.HasName("UpdateDictCache", "dbo"))
-            .Delete(u => u.HasName("DeleteDictCache", "dbo")));
-            builder.Entity<SubType>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertSubType", "dbo"))
-            .Update(u => u.HasName("UpdateSubType", "dbo"))
-            .Delete(u => u.HasName("DeleteSubType", "dbo")));
-            builder.Entity<LearnVoca>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertLearnVoca", "dbo"))
-            .Update(u => u.HasName("UpdateLearnVoca", "dbo"))
-            .Delete(u => u.HasName("DeleteLearnVoca", "dbo")));
-            builder.Entity<ToiecGroup>().MapToStoredProcedures(s => s.Insert(u => u.HasName("InsertToiecGroup", "dbo"))
-            .Update(u => u.HasName("UpdateToiecGroup", "dbo"))
-            .Delete(u => u.HasName("DeleteToiecGroup", "dbo")));
+            builder.Entity<Comment>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertComment", "dbo"))
+                .Update(u => u.HasName("UpdateComment", "dbo"))
+                .Delete(u => u.HasName("DeleteComment", "dbo")));
+            builder.Entity<PostVoteDetail>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertPostVoteDetail", "dbo"))
+                .Update(u => u.HasName("UpdatePostVoteDetail", "dbo"))
+                .Delete(u => u.HasName("DeletePostVoteDetail", "dbo")));
+            builder.Entity<CommentVoteDetail>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertCommentVoteDetail", "dbo"))
+                .Update(u => u.HasName("UpdateCommentVoteDetail", "dbo"))
+                .Delete(u => u.HasName("DeleteCommentVoteDetail", "dbo")));
+            builder.Entity<DetailOurWord>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertDetailOurWord", "dbo"))
+                .Update(u => u.HasName("UpdateDetailOurWord", "dbo"))
+                .Delete(u => u.HasName("DeleteDetailOurWord", "dbo")));
+            builder.Entity<PostType>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertPostType", "dbo"))
+                .Update(u => u.HasName("UpdatePostType", "dbo"))
+                .Delete(u => u.HasName("DeletePostType", "dbo")));
+            builder.Entity<DetailUserType>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertDetailUserType", "dbo"))
+                .Update(u => u.HasName("UpdateDetailUserType", "dbo"))
+                .Delete(u => u.HasName("DeleteDetailUserType", "dbo")));
+            builder.Entity<HaveSendQuestion>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertHaveSendQuestion", "dbo"))
+                .Update(u => u.HasName("UpdateHaveSendQuestion", "dbo"))
+                .Delete(u => u.HasName("DeleteHaveSendQuestion", "dbo")));
+            builder.Entity<DictCache>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertDictCache", "dbo"))
+                .Update(u => u.HasName("UpdateDictCache", "dbo"))
+                .Delete(u => u.HasName("DeleteDictCache", "dbo")));
+            builder.Entity<SubType>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertSubType", "dbo"))
+                .Update(u => u.HasName("UpdateSubType", "dbo"))
+                .Delete(u => u.HasName("DeleteSubType", "dbo")));
+            builder.Entity<LearnVoca>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertLearnVoca", "dbo"))
+                .Update(u => u.HasName("UpdateLearnVoca", "dbo"))
+                .Delete(u => u.HasName("DeleteLearnVoca", "dbo")));
+            builder.Entity<ToiecGroup>().MapToStoredProcedures(s => s
+                .Insert(u => u.HasName("InsertToiecGroup", "dbo"))
+                .Update(u => u.HasName("UpdateToiecGroup", "dbo"))
+                .Delete(u => u.HasName("DeleteToiecGroup", "dbo")));
         }
         //st3. -- END
 
 
     }
 
-        //TODO 1_ThemBang: st4. vào package manager console gõ Add_Migration [Tên_migration tên gì cũng được miễn sao dễ hiểu]
-        //tiếp tục gõ Update_Database
-        //TODO 1_ThemBang: st5....................................................................................End 
+    //TODO 1_ThemBang: st4. vào package manager console gõ Add_Migration [Tên_migration tên gì cũng được miễn sao dễ hiểu]
+    //tiếp tục gõ Update_Database
+    //TODO 1_ThemBang: st5....................................................................................End 
 
-        //TODO 2_CSCot: st2. vào package manager console gõ Add_Migration [Tên_migration tên gì cũng được miễn sao dễ hiểu]
-        //tiếp tục gõ Update_Database
-        //TODO 2_CSCot: st3....................................................................................End
-    
+    //TODO 2_CSCot: st2. vào package manager console gõ Add_Migration [Tên_migration tên gì cũng được miễn sao dễ hiểu]
+    //tiếp tục gõ Update_Database
+    //TODO 2_CSCot: st3....................................................................................End
+
 }
